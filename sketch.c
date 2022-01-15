7e72ed0382fe847933b53ffd947468f521673758
websiteriesen <49904400+websiteriesen@users.noreply.github.com>
2022-01-15 22:53:50 +0100

    added decimalmap for LED stripes

--- sketch.c
+++ sketch.c
@@ -183,3 +183,7 @@ float getSunriseByDayOfYear(int dayOfYear) {
   }
   return 6.750f; 
 }
+
+float decimalmap(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
